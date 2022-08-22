3583f22a3550765d496d5dbbdee8a55635d98e2f
Hafiz <109101725+MohammadHafizChowdhury@users.noreply.github.com>
2022-08-22 07:11:06 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,137 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import styled from "styled-components";
+import MovieComponent from "./components/MovieComponent";
+import MovieInfoComponent from "./components/MovieInfoComponent";
+
+export const API_KEY = "f66cff2e";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-weight: bold;
+  margin-right: 650px;
+  width: 20%;
+`;
+
+const LogIn = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: left;
+`;
+
+const Header = styled.div`
+  background-color: red;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 5px 5px;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 20%;
+  background-color: white;
+`;
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+
+
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
+const MovieListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 25px;
+  justify-content: space-evenly;;
+`;
+const Placeholder = styled.img`
+  width: 300px;
+  height: 200px;
+  margin: 150px;
+  opacity: 80%;
+  
+`;
+
+function App() {
+  const [searchQuery, updateSearchQuery] = useState("");
+
+  const [movieList, updateMovieList] = useState([]);
+  const [selectedMovie, onMovieSelect] = useState();
+
+  const [timeoutId, updateTimeoutId] = useState();
+
+  const fetchData = async (searchString) => {
+    const response = await Axios.get(
+      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`,
+    );
+    updateMovieList(response.data.Search);
+  };
+
+  const onTextChange = (e) => {
+    onMovieSelect("")
+    clearTimeout(timeoutId);
+    updateSearchQuery(e.target.value);
+    const timeout = setTimeout(() => fetchData(e.target.value), 500);
+    updateTimeoutId(timeout);
+  };
+  return (
+    <Container>
+      <Header>
+        <AppName>
+        🎥Cholochitro
+        </AppName>
+        <LogIn>
+          Sign Up
+        </LogIn>
+        <SearchBox>
+          <SearchIcon src="/cholochitro/search-icon.svg" />
+          <SearchInput
+            placeholder="Khoj,The Search"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
+        </SearchBox>
+      </Header>
+      {selectedMovie && <MovieInfoComponent selectedMovie={selectedMovie} onMovieSelect={onMovieSelect}/>}
+      <MovieListContainer>
+        {movieList?.length ? (
+          movieList.map((movie, index) => (
+            <MovieComponent
+              key={index}
+              movie={movie}
+              onMovieSelect={onMovieSelect}
+            />
+          ))
+        ) : (
+          <Placeholder src="/cholochitro/movie-icon.svg" />
+        )}
+      </MovieListContainer>
+    </Container>
+  );
+}
+
+export default App;
